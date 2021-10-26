1e04d977e06cc3c7bd2ba4e147852c91b6748877
yaozj <yaozj@shencai-china.com>
2021-10-26 14:22:10 +0800

    finish context

--- src/main/java/com/hemic/one/config/LocaleConfiguration.java
+++ src/main/java/com/hemic/one/config/LocaleConfiguration.java
@@ -1,9 +1,12 @@
 package com.hemic.one.config;
 
+import java.util.Locale;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import tech.jhipster.config.locale.AngularCookieLocaleResolver;
 
@@ -12,9 +15,9 @@ public class LocaleConfiguration implements WebMvcConfigurer {
 
     @Bean
     public LocaleResolver localeResolver() {
-        AngularCookieLocaleResolver cookieLocaleResolver = new AngularCookieLocaleResolver();
-        cookieLocaleResolver.setCookieName("NG_TRANSLATE_LANG_KEY");
-        return cookieLocaleResolver;
+        AcceptHeaderLocaleResolver acceptHeaderLocaleResolver= new AcceptHeaderLocaleResolver();
+        acceptHeaderLocaleResolver.setDefaultLocale(Locale.ENGLISH);
+        return acceptHeaderLocaleResolver;
     }
 
     @Override
